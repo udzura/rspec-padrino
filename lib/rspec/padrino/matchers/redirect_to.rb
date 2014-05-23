585ebe4faccddbfcd19b37384a8f26f4e6b0cae7
namusyaka <namusyaka@gmail.com>
2014-05-23 18:30:33 +0900

    Use instance variables

--- lib/rspec/padrino/matchers/redirect_to.rb
+++ lib/rspec/padrino/matchers/redirect_to.rb
@@ -3,20 +3,18 @@ module RSpec::Padrino::Matchers
     extend RSpec::Matchers::DSL
 
     matcher :redirect_to do |*expected|
-      expected_url = actual_url = nil
-
       match do |response|
-        expected_url = last_application.absolute_url(*expected)
-        actual_url = response.location
-        response.redirect? && expected_url == actual_url
+        @expected_url = last_application.absolute_url(*expected)
+        @actual_url = response.location
+        response.redirect? && @expected_url == @actual_url
       end
 
       failure_message_for_should do |actual|
-        "expected #{actual} to be a redirect to #{expected_url}, got #{actual_url.inspect}"
+        "expected #{actual} to be a redirect to #{@expected_url}, got #{@actual_url.inspect}"
       end
 
       failure_message_for_should_not do |actual|
-        "expected #{actual} not to be a redirect to #{expected_url}, got #{actual_url.inspect}"
+        "expected #{actual} not to be a redirect to #{@expected_url}, got #{@actual_url.inspect}"
       end
     end
   end
